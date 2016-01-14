75db48e6e7086dbe45a9cf0682c619c74c718229
Chris Maury <chrmaury@gmail.com>
2016-01-14 08:10:35 -0500

    add console log to view

--- SenseOS=Demo/ViewController.swift
+++ SenseOS=Demo/ViewController.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var ConsolLog: UITextView!
+    
+    override func viewDidAppear(animated: Bool) {
+        ConsolLog.text = "\(headset.accelerometerData)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
